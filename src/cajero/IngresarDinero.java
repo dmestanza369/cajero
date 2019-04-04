9c23e6aea1b950cd4d3ba5d29589563b1c89fcaf
AlvaroRio <49152212+AlvaroRio@users.noreply.github.com>
2019-04-04 19:48:23 +0200

    Archivos de Álvaro

---
+++ src/cajero/IngresarDinero.java
@@ -0,0 +1,16 @@
+
+package cajero;
+
+import java.util.Scanner;
+
+
+public class IngresarDinero {
+    
+
+    public int sumardinero(){
+        System.out.println("Cuanto dinero desea ingresar");
+        Scanner sc= new Scanner(System.in);
+        int dinero = sc.nextInt();
+    return dinero;
+    }
+}
---
+++ src/cajero/RecargarTelefono.java
@@ -0,0 +1,44 @@
+
+package cajero;
+
+import java.util.Scanner;
+
+
+public class RecargarTelefono {
+ 
+    int codigo= (int) (Math.random()*9999+1); 
+    Scanner sc = new Scanner(System.in); 
+    
+    public String pedirnumero (){
+        System.out.println("Escriba el numero de telefono que desea recargar:");
+        String telf=sc.next();
+        int tamaño=telf.length();
+        if (tamaño!=9){
+            System.out.println("El numero de telefono escrito no coincide con ningún telefono :,(");
+            telf="0";
+        }
+        return telf;
+    }
+   
+    public void sms (){
+        System.out.println("Le hemos enviado un sms con el codigo de verificación");
+        System.out.println("              ------------------------");
+        System.out.println("              |         "+codigo+"          |");
+        System.out.println("              ------------------------");
+    }
+   public boolean comprobar (){
+       System.out.println("Escriba el codigo de verificación");
+       int codigo1=sc.nextInt();
+       if (codigo1==codigo){
+           System.out.println("Codigo correcto :)");
+           return true;
+       } else {
+           return false;
+       }
+   }
+   public int recargar(){
+       System.out.println("Escriba la cantidad a recargar");
+       int dinero=sc.nextInt();
+       return dinero;
+   }
+}
