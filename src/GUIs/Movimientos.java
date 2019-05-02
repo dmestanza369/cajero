2bbb433260f0f79f583c64eef717ef48e1fbac21
LAPTOP-AQQP1KLG\david <david.mestanza@gmail.com>
2019-05-02 18:57:51 +0200

    Saldo actual

--- src/GUIs/Movimientos.java
+++ src/GUIs/Movimientos.java
@@ -26,6 +26,7 @@ public class Movimientos extends javax.swing.JPanel {
         this.numeroCuenta = cuenta;
         this.controller = cajero.getController();
         //Fuentes
+        Font Consolas14 = new java.awt.Font("Consolas", 0, 14);
         Font Consolas20 = new java.awt.Font("Consolas", 0, 20);
         Font Consolas24 = new java.awt.Font("Consolas", 0, 24);
         
@@ -34,12 +35,20 @@ public class Movimientos extends javax.swing.JPanel {
         
         jButton1.setFont(Consolas20);
         jButton1.setText("ACEPTAR");
-        jTextArea1.setFont(Consolas20);
+        jTextArea1.setFont(Consolas14);
         jTextArea1.setEditable(false);
+        
+        jLabel2.setFont(Consolas14);
+        jLabel2.setText("");
     }
     
     public void addMovimientos(){
-        jTextArea1.setText(this.controller.getStringMovimientos(numeroCuenta));
+        String texto = this.controller.getStringMovimientos(numeroCuenta);
+        if (!"".equals(texto))
+            jTextArea1.setText(texto);
+        else jTextArea1.setText("NO HAY MOVIMIENTOS.");
+        
+        jLabel2.setText("SALDO ACTUAL: " + this.controller.getSaldo(numeroCuenta));
     }
 
     /**
@@ -56,6 +65,7 @@ public class Movimientos extends javax.swing.JPanel {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
@@ -69,20 +79,28 @@ public class Movimientos extends javax.swing.JPanel {
 
         jPanel1.setPreferredSize(new java.awt.Dimension(400, 100));
 
-        jPanel6.setPreferredSize(new java.awt.Dimension(350, 220));
+        jPanel6.setMinimumSize(new java.awt.Dimension(350, 300));
+        jPanel6.setPreferredSize(new java.awt.Dimension(350, 300));
 
         jPanel2.setPreferredSize(new java.awt.Dimension(320, 200));
         jPanel2.setLayout(new java.awt.BorderLayout());
 
+        jScrollPane1.setMaximumSize(new java.awt.Dimension(200, 200));
+        jScrollPane1.setMinimumSize(new java.awt.Dimension(200, 200));
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(200, 200));
+
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
-        jTextArea1.setMinimumSize(new java.awt.Dimension(150, 150));
-        jTextArea1.setPreferredSize(new java.awt.Dimension(600, 1000));
+        jTextArea1.setMinimumSize(new java.awt.Dimension(200, 190));
+        jTextArea1.setPreferredSize(new java.awt.Dimension(200, 190));
         jScrollPane1.setViewportView(jTextArea1);
 
-        jPanel2.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+        jPanel2.add(jScrollPane1, java.awt.BorderLayout.PAGE_END);
 
         jPanel6.add(jPanel2);
+
+        jLabel2.setText("jLabel2");
+        jPanel6.add(jLabel2);
 
         jPanel1.add(jPanel6);
 
@@ -117,6 +135,7 @@ public class Movimientos extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
